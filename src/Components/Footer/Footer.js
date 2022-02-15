32b87dd94f215aafb4bdefb5e6c6f8aee18c64dc
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:15:28 +0300

    feat:add footer top nav

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import '../../index.css';
+import facebook from '../Navbar/assets/facebook.png';
+import twitter from '../Navbar/assets/twitter.png';
+import instagram from '../Navbar/assets/instagram.png';
+import pinterest from '../Navbar/assets/pinterest.png';
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className='footer' data-test-id='footer'>
+            <div className='footer_block__top'>
+                <div className='layouts-3-columns wrapper'>
+                    <h3 className='form_title'>be in touch with us:</h3>
+                    <div className='form'>
+                        <input type="email" name="email" id="email" placeholder="Enter your email" />
+                        <button className='footer_btn btn'>join us</button>
+                    </div>
+                    <div className='social_links'>
+                        <ul class="nav_top__icon">
+                            <li><img src={facebook} alt="fb" /></li>
+                            <li><img src={twitter} alt="tw" /></li>
+                            <li><img src={instagram} alt="inst" /></li>
+                            <li><img src={pinterest} alt="pint" /></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+        </footer>
+
+    );
+
 }
 
-export default Footer
+export default Footer;
