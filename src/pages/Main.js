17bead16fec5ffc2da0e350b473dca4a24e7308c
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:08:04 +0300

    feat:create component footer

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 
+import Footer from '../components/Footer/Footer';
+
 function Main() {
   return (
     <div>
       <Navbar />
+      
+      <Footer />
     </div>
   )
 }
