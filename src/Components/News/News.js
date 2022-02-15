57c8cb3641d6b65fceb813e29c933c831c86ce49
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:57:34 +0300

    feat: create component news

---
+++ src/Components/News/News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './News.css';
+import '../Header/Header.css'
+
+
+function News() {
+  return (
+    <div>News</div>
+  )
+}
+
+export default News
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Header from '../components/Header/Header';
 
+import News from '../components/News/News';
 import Special from '../components/Special/Special';
 import Blog from '../components/Blog/Blog';
 import Footer from '../components/Footer/Footer';
@@ -12,6 +13,7 @@ function Main() {
       <Navbar />
       <Header />
 
+      <News />
       <Special />
       <Blog />
       <Footer />
