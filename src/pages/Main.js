a26bb8f6aea240ababbe22e453a6f0e99ec8e257
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:26:03 +0300

    feat:add folder,icons for header

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
+import Header from '../components/Header/Header';
 
 import Footer from '../components/Footer/Footer';
 
@@ -7,6 +8,8 @@ function Main() {
   return (
     <div>
       <Navbar />
+      <Header />
+
       
       <Footer />
     </div>
