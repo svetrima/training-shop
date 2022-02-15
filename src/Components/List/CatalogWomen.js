5855b6bce960f6a5eb7ec1acb9f0afaf1fd64306
Svetrima <svetsmetanina@gmail.com>
2022-02-16 02:18:25 +0300

    feat:add array data in catalogwomen

---
+++ src/Components/List/CatalogWomen.js
@@ -0,0 +1,68 @@
+export const women = [
+
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109.jpg'),
+        category: 'women',
+        id: 1
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_2.jpg'),
+        category: 'women',
+        id: 2
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_3.jpg'),
+        category: 'women',
+        id: 3
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_4.jpg'),
+        category: 'women',
+        id: 4
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_5.jpg'),
+        category: 'women',
+        id: 5
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_6.jpg'),
+        category: 'women',
+        id: 6
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_7.jpg'),
+        category: 'women',
+        id: 7
+    },
+    {
+        title: "Women's tracksuit Q109",
+        price: '$ 30.00',
+        rating: require('./assets/rating.png'),
+        img: require('./assets/women/tracksuitQ109_8.jpg'),
+        category: 'women',
+        id: 8
+    },
+
+]
