d8b2125b4bdee79c2b2bc072bb1ec6d0208bdce7
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:11:09 +0300

    feat:fill data files for footer

---
+++ src/Components/Footer/Information.js
@@ -0,0 +1,24 @@
+export const information = [
+    {
+        id:1,
+        name:'About Us',
+        path:'About Us',
+    },
+    {
+        id:2,
+        name:'Contact Us',
+        path:'Contact Us',
+    },
+    {
+        id:3,
+        name:'Blog',
+        path:'Blog',
+    },
+    
+    {
+        id:4,
+        name:'FAQs',
+        path:'FAQs',
+    },
+   
+]
