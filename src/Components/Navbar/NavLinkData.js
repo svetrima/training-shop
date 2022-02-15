13a5945a219aeaa62f236cfe35719bb798a63d41
Svetrima <svetsmetanina@gmail.com>
2022-02-15 22:39:52 +0300

    feat:create navlinkdata for link

--- src/Components/Navbar/NavLinkData.js
+++ src/Components/Navbar/NavLinkData.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
-function NavLinkData() {
-  return (
-    <div>NavLinkData</div>
-  )
-}
-
-export default NavLinkData
+export const NavMenu = [
+    {
+        id:1,
+        name:'About Us',
+        path:'About Us',
+    },
+    {
+        id:2,
+        name:'Women',
+        path:'Women',
+    },
+    {
+        id:3,
+        name:'Men',
+        path:'Men',
+    },
+    {
+        id:4,
+        name:'Beauty',
+        path:'NotFoundPage',
+    },
+    {
+        id:5,
+        name:'Accessories',
+        path:'NotFoundPage',
+    },
+    {
+        id:6,
+        name:'Blog',
+        path:'NotFoundPage',
+    },
+    {
+        id:7,
+        name:'Contact',
+        path:'Product',
+    },
+]
