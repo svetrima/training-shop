f93258b23b6ee92e9c8034553fedda79b703823a
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:37:48 +0300

    feat:add markup and styles for slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Slider.css';
 
 function Slider() {
-  return (
-    <div>Slider</div>
-  )
+    return (
+        <div className='slider'>
+            <div className='arrow arrow_left'>
+            </div>
+            <div className='arrow arrow_right'>
+            </div>
+        </div>
+
+    )
 }
 
 export default Slider
