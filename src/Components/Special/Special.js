9cd5b5ab9fea96c886bb1c33e103374162ceb6ab
Svetrima <svetsmetanina@gmail.com>
2022-02-15 23:52:01 +0300

    feat:add markup for special

--- src/Components/Special/Special.js
+++ src/Components/Special/Special.js
@@ -4,9 +4,19 @@ import '../../index.css';
 
 
 function Special() {
-  return (
-    <div>Special</div>
-  )
+    return (
+        <div className='special wrapper'>
+            <div className='offer'>
+                <h3>Special Offer</h3>
+                <p>Subscribe And <span className='sales'>Get 10% Off</span></p>
+                <input type="email" name="email" id="email" placeholder="Enter your email" />
+                <button className='offer_btn btn'>subscribe</button>
+
+            </div>
+            <div className='for_women'></div>
+            <div className='for_men'></div>
+        </div>
+    );
 }
 
 export default Special
