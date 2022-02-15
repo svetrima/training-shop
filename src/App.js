ae0a31cbc48ab5b203a41a52c7de02515539b4fb
Svetrima <svetsmetanina@gmail.com>
2022-02-15 22:56:20 +0300

    feat:connection routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import  Main  from './pages/Main';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App" data-test-id="app">
+      <Routes>
+        <Route index element={<Main />} />
+      </Routes>
     </div>
   );
 }
