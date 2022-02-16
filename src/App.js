9d0c4e509e5f29077351a5490d826a50a08d1a8e
Svetrima <svetsmetanina@gmail.com>
2022-02-16 04:22:21 +0300

    feat:add folder. files for other pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import  Main  from './pages/Main';
+import Main from './pages/Main';
+import PageWomen from './pages/Category/PageWomen';
+import PageMen from './pages/Category/PageMen';
+import NotFoundPage from './pages/NotFoundPage';
+import Product from './pages/Product/Product';
 
 function App() {
   return (
     <div className="App" data-test-id="app">
       <Routes>
         <Route index element={<Main />} />
-        
+        <Route path="/Women" element={<PageWomen />} />
+        <Route path="/Men" element={<PageMen />} />
+        <Route path="/NotFoundPage" element={<NotFoundPage />} />
+        <Route path="/Product" element={<Product />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+
+function NotFoundPage() {
+  return (
+    <div>
+      <Navbar />
+      I'm sorry
+      <br />
+      404
+      <br />
+      Have a nice day
+      <Footer />
+    </div>
+  );
+}
+
+export default NotFoundPage
