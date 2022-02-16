c69903d25d89016b011f21f360628e3e49774bbd
Svetrima <svetsmetanina@gmail.com>
2022-02-16 15:17:22 +0300

    feat:add styles. markup, icon

---
+++ src/pages/Product/Info.js
@@ -0,0 +1,14 @@
+export const info = [
+    {
+        name: 'Color',
+        detail: 'Blue, White, Black, Grey'
+    },
+    {
+        name: 'Size',
+        detail: 'XS, S, M, L',
+    },
+    {
+        name: 'Material',
+        detail: '	100% Polyester'
+    }
+]
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import './Product.css';
+import './Product.css';
 import '../../index.css';
 import '../Category/Category.css';
 import Breadcrumbs from '../Category/Breadcrumbs';
@@ -11,7 +11,7 @@ import hanger from './assets/icons/hanger.png';
 import scales from './assets/icons/scales.png';
 import exchange from '../../Components/Header/assets/refresh.png';
 import delivery from '../../Components/Header/assets/truck.png';
-import support from '../../Components/Header/assets/support.png';
+import mail from '../../Components/Navbar/assets/mail.png';
 import stripe from './assets/icons/Stripe_x42.png';
 import aes from './assets/icons/aes_x42.png';
 import paypal from './assets/icons/paypal_2_x42.png';
@@ -33,6 +33,8 @@ import choiceActive from './assets/choise_active.png';
 import choice1 from './assets/choice_2.png';
 import choice2 from './assets/choice_3.png';
 import choice3 from './assets/choice_4.png';
+import message from './assets/icons/annotation.png';
+import { info } from './Info';
 
 
 function Product() {
@@ -71,19 +73,32 @@ function Product() {
                         <div className='gallery_description'>
                             <div className='clothes_description'>
                                 <div className='clothes_description__color'>
-                                    <h4>color:<span>blue</span></h4>
-                                    <img src={choiceActive} alt='active' />
-                                    <img src={choice1} alt='choice1' />
-                                    <img src={choice2} alt='choice2' />
-                                    <img src={choice3} alt='choice3' />
+                                    <div className='color_title'>
+                                        <h4>color:</h4>
+                                        <span className='colorful_title'>Blue</span>
+                                    </div>
+                                    <div className='color_choice'>
+                                        <img src={choiceActive} alt='active' />
+                                        <img src={choice1} alt='choice1' />
+                                        <img src={choice2} alt='choice2' />
+                                        <img src={choice3} alt='choice3' />
+                                    </div>
                                 </div>
                                 <div className='clothes_description__size'>
-                                    <h4>size:<span>s</span></h4>
-                                    <img src={sizeXS} alt='sizeXS' />
-                                    <img src={sizeActive} alt='active' />
-                                    <img src={sizeM} alt='sizeM' />
-                                    <img src={sizeL} alt='sizeL' />
-                                    <span><img src={hanger} alt='hanger' /> Size quide</span>
+                                    <div className='size_title'>
+                                        <h4>size:</h4>
+                                        <span className='size'>S</span>
+                                    </div>
+                                    <div className='size_choice'>
+                                        <img src={sizeXS} alt='sizeXS' />
+                                        <img src={sizeActive} alt='active' />
+                                        <img src={sizeM} alt='sizeM' />
+                                        <img src={sizeL} alt='sizeL' />
+
+                                    </div>
+                                    <div className='size_guide'>
+                                        <span><img src={hanger} alt='hanger' /> Size quide</span>
+                                    </div>
                                 </div>
                             </div>
                             <div className='gallery_description__price'>
@@ -92,21 +107,24 @@ function Product() {
                                 <span><img src={like} alt='like' /></span>
                                 <span><img src={scales} alt='scales' /></span>
                             </div>
-                            <div className='gallery_description__delivery layouts-3-columns'>
-                                <img src={delivery} alt='delivery' />
-                                <div className='delivery'>
-                                    <h3>Shipping &amp; Delivery</h3>
-                                </div>
-                                <img src={exchange} alt='exchange' />
-                                <div className='exchange'>
-                                    <h3>Returns &amp; Exchanges</h3>
-                                </div>
-                                <img src={support} alt='support' />
-                                <div className='support'>
-                                    <h3>Ask a question</h3>
+                            <div className='gallery_description__delivery'>
+                                <div className='layouts-3-columns'>
+                                    <img src={delivery} alt='delivery' />
+                                    <div className='delivery'>
+                                        <h3>Shipping &amp; Delivery</h3>
+                                    </div>
+                                    <img src={exchange} alt='exchange' />
+                                    <div className='exchange'>
+                                        <h3>Returns &amp; Exchanges</h3>
+                                    </div>
+                                    <img src={mail} alt='mail' />
+                                    <div className='mail'>
+                                        <h3>Ask a question</h3>
+                                    </div>
                                 </div>
                             </div>
                             <div className='gallery_description__checkout'>
+                                <h4>guaranteed safe checkout</h4>
                                 <ul className='pay_links'>
                                     <li><img src={stripe} alt="stripe" /></li>
                                     <li><img src={aes} alt="aes" /></li>
@@ -118,16 +136,69 @@ function Product() {
                                 </ul>
                             </div>
                             <div className='gallery_description__info'>
-
+                                <h3>description</h3>
+                                <div className='description_details'>
+                                    <h3>additional information</h3>
+                                    <ul>
+                                        {info.map(element => {
+                                            return <li><span className='description_details__title'>{element.name}:</span><span className='description_details__content'>{element.detail}</span></li>
+                                        })}
+                                    </ul>
+                                </div>
                             </div>
                             <div className='gallery_description__review'>
                                 <div className='review_info'>
-                                    <img src={fullrating} alt='rating' />
-                                    <span className='count_review'></span>
+                                    <h3>reviews</h3>
+                                    <div className='review_sub layouts-2-columns'>
+                                    <div className='rating_info'>
+                                        <img src={fullrating} alt='rating' />
+                                        <span className='count_review'>2 reviews</span>
+                                        </div>
+                                        <div className='new_review'>
+                                    <img src={message} alt='message' />
+                                    <span className='add_review'>Write a review</span>
+
                                 </div>
-                                <div className='new_review'>
+                                    </div>
+                                    <div className='review_block'>
+                                    <div className='review_block__title layouts-2-columns'>
+                                        <h4 className='user_title'>Oleh Chabanov</h4>
+                                        <div className='rating_info'>
+                                        <span className='date_review'>3 months ago</span>
+                                        <img src={fullrating} alt='rating' /></div>
+                                        </div>
+                                        <div className='user_content'>
+                                            <p>On the other hand, we denounce with righteous indignation and like men who are so beguiled and demoralized by the charms of pleasure of the moment</p>
+                                        </div>
+                                        
+                                    </div>
+                                    <div className='review_block'>
+                                        <div className='review_block__title layouts-2-columns'>
+                                        <h4 className='user_title'>ShAmAn design</h4>
+                                        <div className='rating_info'>
+                                        <span className='date_review'>3 months ago</span>
+                                        <img src={fullrating} alt='rating' />
+                                        </div>
+                                        </div>
+                                        
+                                        
+                                        <div className='user_content'>
+                                          <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti</p>
+                                        </div>
+                                        
+                                    </div>
                                 </div>
+                              
                             </div>
+                        </div>
+                    </div>
+                    <div className='gallery_related'>
+                        <div className='gallery_related__title layouts-2-columns'>
+                        <h2>related products</h2>
+                        <div className='paginaition'>
+                        <div className='arrow_prev'>left</div>
+                         <div className='arrow_next'>right</div>
+                         </div>
                         </div>
                     </div>
                 </div>
