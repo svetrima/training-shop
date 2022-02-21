5c37394f1518e2ca83a80a01d2b5d6023686ac34
Svetrima <svetsmetanina@gmail.com>
2022-02-22 02:15:06 +0300

    feat: S2 add slider code for main slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import './Slider.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css';
+import slid from '../Header/assets/header_main.jpg';
+import '../Header/Header.css'
+
+
 
 function Slider() {
+
     return (
         <div className='slider'>
-            <div className='arrow arrow_left'>
-            </div>
-            <div className='arrow arrow_right'>
-            </div>
+            <Swiper
+                modules={[Navigation]}
+                navigation={true}
+                className="swiper" data-test-id='main-slider'>
+                <SwiperSlide className='slides'>
+                    <img src={slid} alt={slid} />
+                    <div className='main_block__banner banner'>
+                        <p>banner</p>
+                        <h2>your title text</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='slides'>
+                    <img src={slid} alt={slid} />
+                    <div className='main_block__banner banner'>
+                        <p>banner</p>
+                        <h2>your title text</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='slides'>
+                    <img src={slid} alt={slid} />
+                    <div className='main_block__banner banner'>
+                        <p>banner</p>
+                        <h2>your title text</h2>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+
         </div>
 
     )
+
 }
 
 export default Slider
