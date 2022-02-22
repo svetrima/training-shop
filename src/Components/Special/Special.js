8847c21e14dee85bc2e26b865efa6115189e0700
Svetrima <svetsmetanina@gmail.com>
2022-02-22 16:57:51 +0300

    fix special for adaptive

--- src/Components/Special/Special.js
+++ src/Components/Special/Special.js
@@ -5,14 +5,14 @@ import '../../index.css';
 
 function Special() {
     return (
-        <div className='wrapper'>
-            <div className='special'>
+        <div className='special'>
+            <div className='wrapper'>
                 <div className='offer'>
                     <h3>Special Offer</h3>
                     <p>Subscribe And <span className='sales'>Get 10% Off</span></p>
                     <form>
-                    <input type="email" name="email" id="email" placeholder="Enter your email" />
-                    <button className='offer_btn btn'>subscribe</button>
+                        <input type="email" name="email" id="email" placeholder="Enter your email" />
+                        <button className='offer_btn btn'>subscribe</button>
                     </form>
                 </div>
                 <div className='for_women'></div>
