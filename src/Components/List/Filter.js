73edca4c2acbe90075aa0ba5f108e0a3b3057107
Svetrima <svetsmetanina@gmail.com>
2022-03-02 16:57:43 +0300

    feat: fix build's error, add code for filter

--- src/Components/List/Filter.js
+++ src/Components/List/Filter.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
 import { cardsFilterData } from './CardsFilterData';
+import classNames from "classnames";
 
-const Filter =()=> {
-
+const Filter = () => {
+    const [filter, setFilter] = useState(cardsFilterData[0]);
 
     return (
         <div className='cards_filter'>
             {cardsFilterData.map(element => {
-                
-                 return <span className='filter_tab'>{element.name}</span>;
+                return <span className={classNames('filter_tab', { 'filter_tab__show': element.particularName === filter })} value={element.particularName} onClick={() => setFilter(element.particularName)} >{element.name}</span>;
             })}
-
         </div>
     );
 
