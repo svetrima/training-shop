13347b5176ec9d7529671f1d52e283e4a6b28758
Svetrima <svetsmetanina@gmail.com>
2022-03-02 21:27:24 +0300

    fix: fix first filter's active tab

--- src/Components/List/Filter.js
+++ src/Components/List/Filter.js
@@ -4,12 +4,14 @@ import { cardsFilterData } from './CardsFilterData';
 import classNames from "classnames";
 
 const Filter = () => {
-    const [filter, setFilter] = useState(cardsFilterData[0]);
+    const [filter, setFilter] = useState(cardsFilterData[0].particularName);
 
     return (
         <div className='cards_filter'>
             {cardsFilterData.map(element => {
-                return <span className={classNames('filter_tab', { 'filter_tab__show': element.particularName === filter })} value={element.particularName} onClick={() => setFilter(element.particularName)} >{element.name}</span>;
+                return <span 
+                className={classNames('filter_tab', { 'filter_tab__show': element.particularName === filter})} 
+                value={element.particularName} onClick={() => setFilter(element.particularName)} >{element.name}</span>;
             })}
         </div>
     );
