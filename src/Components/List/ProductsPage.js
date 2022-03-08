a5e802470a7d3be7db15d0d605f21c8ec2d672c8
Svetrima <svetsmetanina@gmail.com>
2022-03-08 12:36:52 +0300

    feat:add discount block

--- src/Components/List/ProductsPage.js
+++ src/Components/List/ProductsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PRODUCTS } from './ProductsData';
 import { Link } from 'react-router-dom';
+import classNames from "classnames";
 
 
 const ProductsPage = ({ typeProducts }) => {
@@ -11,6 +12,9 @@ const ProductsPage = ({ typeProducts }) => {
                     return <Link to={`/${element.category}/${element.id}`} className='cards-item' data-test-id={`clothes-card-${element.category}`}><div className='card'>
                         <img key={element.id} src={`https://training.cleverland.by/shop${element.images[0]?.url}`}  alt={element.title} />
                         <div className='card_content'>
+                        <div className={classNames('discount', {'discount_active':element.discount })}>
+                            <span>{element.discount}</span>
+                            </div>
                             <h4 className='card_item__title'>{element.name}</h4>
                             <div className='card_item__description'>
                                 <span className='card_item__price'>&#36; {element.price}</span>
