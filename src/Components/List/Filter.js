ed2e44693e8c311a4e44e0b4995c8ca277316c2e
Svetrima <svetsmetanina@gmail.com>
2022-03-08 12:35:58 +0300

    feat:add discount block

--- src/Components/List/Filter.js
+++ src/Components/List/Filter.js
@@ -27,6 +27,9 @@ const Filter = ({ typeProducts }) => {
                             return <Link to={`/${element.category}/${element.id}`} className='cards-item' data-test-id={`clothes-card-${element.category}`}><div className='card'>
                                 <img key={element.id} src={`https://training.cleverland.by/shop${element.images[0]?.url}`} alt={element.title} />
                                 <div className='card_content'>
+                                    <div className={classNames('discount', { 'discount_active': element.discount })}>
+                                        <span>{element.discount}</span>
+                                    </div>
                                     <h4 className='card_item__title'>{element.name}</h4>
                                     <div className='card_item__description'>
                                         <span className='card_item__price'>&#36; {element.price}</span>
